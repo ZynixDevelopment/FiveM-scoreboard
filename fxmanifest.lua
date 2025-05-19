94210fd37138fb906128a3af83f4b12cb2c6a2fe
Zynix <162629879+ZynixDevelopment@users.noreply.github.com>
2025-05-19 12:21:06 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+ui_page 'html/scoreboard.html'
+
+files {
+    'html/scoreboard.html',
+    'html/scoreboard.js',
+    'html/scoreboard.css'
+}
